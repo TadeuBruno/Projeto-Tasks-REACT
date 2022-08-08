cf886c475d6ab03f9e50a77816445a57aee8aa4d
TadeuBruno <bruno.correa@germinare.org.br>
2022-08-07 22:15:54 -0300

    commit

---
+++ src/Components/TaskDetails.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import Button from './Button'
+// import {useParams} from "react-router-dom"
+// import Task from './Task'
+// import "./TaskDetails.css"
+
+// const TaskDetails = () => {
+//     const params = useParams();
+//   return (
+//     <div>
+//         <>
+//             <div className="back-button-container">
+//                 <Button>Voltar</Button>
+//             </div>
+//             <div className="task-details-container">
+//                 <h2>{params.taskTitle}</h2>
+//                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eius exercitationem officia, ipsam quidem est.</p>
+//             </div>
+//         </>
+//     </div>
+//   )
+// }
+
+// export default TaskDetails
